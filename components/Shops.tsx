b63f2086be97c3704e81dede01f68f4c7b2a4667
MuuShuu <MuuShuu@users.noreply.github.com>
2023-02-16 21:50:10 +0200

    cosmetic changes

--- components/Shops.tsx
+++ components/Shops.tsx
@@ -1,10 +1,9 @@
-import { FC, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hook';
 import { axiosShops } from '../redux/slice/shopsSlice';
 import Shop from './Shop';
 
-
-const Shops:FC = () => {
+const Shops = () => {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -12,7 +11,7 @@ const Shops:FC = () => {
   }, [dispatch]);
 
   const shops = useAppSelector(state => state.shops.shops)
-  
+
   return (
     <div className="w-1/6 pl-2 min-h-max">
       <div className="border-main flex flex-col items-center bg-gray-200 h-full">
